22988a55ce8392440ebcffea5047c684cc63e078
Ezequiel Pérez <eperez@itba.edu.ar>
2022-06-04 19:07:53 -0300

    chore: non blocking sockets
    Co-authored-by: vyeli <yeliv9527@gmail.com>
    Co-authored-by: gbaliarda <gbaliarda@itba.edu.ar>
    Co-authored-by: nbirsa <nicolasbirsa333@gmail.com>

--- src/trnsProxy.c
+++ src/trnsProxy.c
@@ -13,6 +13,7 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros
+#include <fcntl.h>
 #include "include/buffer.h"
 
 #define TRUE   1
@@ -42,12 +43,10 @@ int main(int argc , char *argv[]) {
     memset(&hints, 0, sizeof(struct addrinfo));
     hints.ai_family = AF_INET;
 
-    // getaddrinfo bloqueante
     if((getaddrinfo("www.google.com", "80", &hints, &remote_servers)) != 0){
         perror("getaddrinfo failed");
         exit(EXIT_FAILURE);
     }
-
 
     client client_socket[MAX_CLIENTS];
     for(int i = 0; i < MAX_CLIENTS; i++) {
@@ -58,16 +57,10 @@ int main(int argc , char *argv[]) {
     }
 
     // create a master socket - socket pasivo
-    // type: SOCK_STREAM | SOCK_NONBLOCK para hacerlo no bloqueante
-    if ((master_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    if ((master_socket = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0)) == 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
-
-    // if((http_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0){
-    //     perror("http socket failed");
-    //     exit(EXIT_FAILURE);
-    // }
   
     // set master socket to allow multiple connections , this is just a good habit, it will work without this
     if (setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0) {
@@ -88,8 +81,8 @@ int main(int argc , char *argv[]) {
     }
     printf("Listening on port %d \n", PORT);
      
-    // try to specify maximum of 3 pending connections for the master socket
-    if (listen(master_socket, 3) < 0) {
+    // try to specify maximum of 5 pending connections for the master socket
+    if (listen(master_socket, 5) < 0) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
@@ -111,6 +104,7 @@ int main(int argc , char *argv[]) {
         FD_SET(master_socket, &readfds);
         int max_sd = master_socket;
         int sd, osfd;
+        int valread;
          
         // check if there are child sockets to set and set them
         for (i = 0; i < MAX_CLIENTS; i++) {
@@ -146,40 +140,38 @@ int main(int argc , char *argv[]) {
           
         // If something happened on the master socket , then its an incoming connection
         if (FD_ISSET(master_socket, &readfds)) {
-            // TODO: hacer no bloqueante?
             if ((new_socket = accept(master_socket, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-                perror("accept");
-                exit(EXIT_FAILURE);
-            }
-          
-            // inform user of socket number - used in send and receive commands
-            printf("New connection , socket fd is %d , ip is : %s , port : %d \n" , new_socket , inet_ntoa(address.sin_addr), ntohs(address.sin_port));
-        
-            // send new connection greeting message
-            if (send(new_socket, WELCOME_MSG, strlen(WELCOME_MSG), 0) != strlen(WELCOME_MSG)) 
-                perror("send");
-              
-            puts("Welcome message sent successfully");
-              
-            // add new socket to array of sockets
-            for (i = 0; i < MAX_CLIENTS; i++) {
-                // if position is empty
-                if (client_socket[i].fd == 0) {
-                    client_socket[i].fd = new_socket;
+                if (errno != EWOULDBLOCK && errno != EAGAIN) {
+                    perror("accept");
+                    exit(EXIT_FAILURE);
+                }
+            } else {
+                // inform user of socket number - used in send and receive commands
+                printf("New connection , socket fd is %d , ip is : %s , port : %d \n" , new_socket , inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+                
+                // add new socket to array of sockets
+                for (i = 0; i < MAX_CLIENTS; i++) {
+                    // if position is empty
+                    if (client_socket[i].fd == 0) {
+                        client_socket[i].fd = new_socket;
+                        fcntl(new_socket, F_SETFL, O_NONBLOCK);
 
-                    // create origin server socket
-                    if ((client_socket[i].originFd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-                        perror("origin socket failed");
-                        exit(EXIT_FAILURE);
+                        // create origin server socket
+                        if ((client_socket[i].originFd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0)) == 0) {
+                            perror("origin socket failed");
+                            exit(EXIT_FAILURE);
+                        }
+                        // connecting to google, if in progress, waiting in select to write
+                        if ((connect(client_socket[i].originFd, remote_servers->ai_addr, sizeof(struct sockaddr))) == -1) {
+                            if (errno != EINPROGRESS && errno != EAGAIN) {
+                                perror("origin server connect failed");
+                                exit(EXIT_FAILURE);
+                            }
+                        }
+
+                        printf("Adding to list of sockets as %d\n" , i);
+                        break;
                     }
-
-                    if((connect(client_socket[i].originFd, remote_servers->ai_addr, sizeof(struct sockaddr))) == -1) {
-                        perror("origin server connect failed");
-                        exit(EXIT_FAILURE);
-                    }
-
-                    printf("Adding to list of sockets as %d\n" , i);
-                    break;
                 }
             }
         }
@@ -193,70 +185,56 @@ int main(int argc , char *argv[]) {
             buffer *bw = &client_socket[i].writeBuffController;
             size_t wbytes;
             size_t rbytes;
-            int valread;
               
             if (FD_ISSET(sd, &readfds)) {
                 wbytes = bufferFreeSpace(br);
                 // check if it was for closing
-                if ((valread = read(sd, getWritePtr(br), wbytes)) == 0) {
+                if ((valread = recv(sd, getWritePtr(br), wbytes, 0)) == 0) {
                     // somebody disconnected, get his details and print
                     getpeername(sd , (struct sockaddr*)&address, (socklen_t*)&addrlen);
                     printf("Host disconnected , ip %s , port %d \n", inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
                       
                     // close the socket and mark as 0 in list for reuse
                     close(sd);
+                    close(osfd);
                     client_socket[i].fd = 0;
+                    client_socket[i].originFd = 0;
                 }
-                // else echo back the message that came in
-                else {
-                    // send(http_socket, request, strlen(request), 0);
-                    // while(read(http_socket, response, 4999) > 0){
-                    //     printf("%s", response);
-                    // }
-                    /*
-                    printf("Host www.google.com:\n");
-                    for(tmp = remote_servers ; tmp != NULL ; tmp = tmp->ai_next){
-                        inet_ntop(tmp->ai_family, tmp->ai_addr->sa_data, server_ip, 50);
-                        ptr = &((struct sockaddr_in *)tmp->ai_addr)->sin_addr;
-                        inet_ntop(tmp->ai_family, ptr, server_ip, 50);
-                        printf("- Host #%d: %s\n", i, server_ip);
-                        i++;
-                    }
-                    */
-                    // confirmamos cant bytes leidos con read
+                else if (valread > 0)
                     advanceWritePtr(br, valread);
-                }
+                else if (errno != EWOULDBLOCK && errno != EAGAIN)
+                    perror("read from client");
             }
 
             if (FD_ISSET(osfd, &readfds)) {
                 wbytes = bufferFreeSpace(bw);
-                // check if it was for closing
                 if ((valread = recv(osfd, getWritePtr(bw), wbytes, 0)) == 0) {
-                    // somebody disconnected, get his details and print
                     getpeername(osfd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
                     printf("Google disconnected , ip %s , port %d \n", inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
-                      
-                    // close the socket and mark as 0 in list for reuse
+                    
+                    close(sd);
                     close(osfd);
+                    client_socket[i].fd = 0;
                     client_socket[i].originFd = 0;
                 }
-                // else echo back the message that came in
-                else {
-                    // confirmamos cant bytes leidos con read
+                else if (valread > 0) 
                     advanceWritePtr(bw, valread);
-                }
+                else if (errno != EWOULDBLOCK && errno != EAGAIN)
+                    perror("read from origin");
             }
 
             if (FD_ISSET(osfd, &writefds)) {
-                rbytes = send(osfd, getReadPtr(br), bufferPendingRead(br), 0);
-                // confirmamos cant bytes leidos con send
-                advanceReadPtr(br, rbytes);
+                if((rbytes = send(osfd, getReadPtr(br), bufferPendingRead(br), 0)) >= 0)
+                    advanceReadPtr(br, rbytes);
+                else if (errno != EWOULDBLOCK && errno != EAGAIN)
+                    perror("write to origin");
             }
 
             if (FD_ISSET(sd, &writefds)) {
-                rbytes = send(sd, getReadPtr(bw), bufferPendingRead(bw), 0); // OJO: ESTO PODRIA BLOQUEARSE si el cliente no consume y se llena el buffer de salida
-                // confirmamos cant bytes leidos con send()
-                advanceReadPtr(bw, rbytes);
+                if ((rbytes = send(sd, getReadPtr(bw), bufferPendingRead(bw), 0)) >= 0)
+                    advanceReadPtr(bw, rbytes);
+                else if (errno != EWOULDBLOCK && errno != EAGAIN)
+                    perror("write to client");
             }
         }
     }
