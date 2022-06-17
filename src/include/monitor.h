78c5a8a0dbe56fc44e80d73ee228c5f5ea47c6f6
vyeli <yeliv9527@gmail.com>
2022-06-16 22:04:21 -0300

    feat: monitor protocol parser server side

--- src/include/monitor.h
+++ src/include/monitor.h
@@ -5,52 +5,69 @@
 #include <stdbool.h>
 #include "buffer.h"
 
+#define TOKEN_SIZE 17  // LE PUSE 17 ASI PONGO EL NULL TERMINATED
+#define DATA_SIZE 8192
+#define USERNAME_SIZE 256
+#define PASSWORD_SIZE 256
+
 /** Docs: https://docs.google.com/document/d/11LOlBxNPXL2N811n5hB9y6pTDAw9zajFgFRj6j-yLTM/edit */ 
 
-enum monitor_req_method {
-    method_get = 0x00,
-    method_config = 0x01,
-};
+/**
+ * Request
 
-enum monitor_req_target_get {
-    target_get_historic = 0x00,
-    target_get_concurrent = 0x01,
-    target_get_transfered = 0x02,
-    // target_get_uptime = 0x02,
-};
+    VER | TOKEN | MÉTODO | TARGET | DLEN | DATA
+    1       16      1        1       2      1 to 8191
 
-enum monitor_req_target_config_opt {
-    target_config_pop3disector = 0x00,
-};
 
-struct monitor_req_target_config {
-    enum monitor_req_target_config_opt option;
-    bool (*isValid)(struct monitor_parser *);
-};
+VERSIÓN:
+Este campo DEBE ser X' 01' .
 
-union monitor_req_target {
-    enum monitor_req_target_get get;
-    struct monitor_req_target_config config;
-};
+TOKEN: Este campo DEBE ser un código de 16 caracteres usado para acceder al server de monitoreo.
 
-struct monitor {
-    enum    monitor_req_method     method;
-    union   monitor_req_target     target;
-    uint8_t                        dlen;
-    uint8_t                        data[0xFF];
-};
+MÉTODO:
+    X'00' GET
+    X'01' CONFIG
+
+TARGET:
+GET
+    X'00'  cantidad de conexiones históricas
+    X'01'  cantidad de conexiones concurrentes
+    X'02'  cantidad de bytes transferidos
+    X'03'  listado de usuarios del proxy
+    X'04'  listado de administradores
+CONFIG
+    X'00'  ON/OFF password disector POP3
+    X'01'  agregar usuario del proxy
+    X'02'  borrar usuarios del proxy
+    X'03'  agregar usuario admin
+    X'04'  borrar usuarios admin
+
+DLEN: cantidad de bytes presentes en la sección DATA. Para el método GET, este campo DEBERÍA ser X'01' .
+
+DATA: 
+    GET
+        Este campo DEBERÍA ser X'00' , pero NO DEBERÍA ser leído por el servidor.
+    CONFIG
+        Pass disector:
+            X'00'  OFF
+            X'01'  ON
+        Agregar usuario del proxy
+            <usuario>X'00'<contraseña> 
+        Borrar usuario proxy
+            <usuario>
+        Agregar usuario admin
+            <usuario>X'00'<token>
+        Borrar usuario admin
+            <usuario>
+*/
 
 enum monitor_state {            
     monitor_version,
+    monitor_token,
     monitor_method,
     monitor_target,
     monitor_dlen,
     monitor_data,
-    monitor_ulen,
-    monitor_uname,
-    monitor_plen,
-    monitor_passwd,
-    monitor_authenticate,
 
     // apartir de aca estan done
     monitor_done,
@@ -58,31 +75,91 @@ enum monitor_state {
     // y apartir de aca son considerado con error 
     monitor_error,
     monitor_error_unsupported_version,
+    monitor_error_invalid_token,
     monitor_error_unsupported_method,
     monitor_error_unsupported_target,
-    monitor_error_unsupported_data,
-    monitor_error_unsupported_auth,
-};
-
-struct monitor_parser {
-    struct monitor *request;
-    enum monitor_state state;
-    /** cuantos bytes tenemos que leer*/            /*Notemos que hay estados que tiene que leer varios bytes, por eso usamos el i de la estructura del request parser para saber cuantos bytes ya leimos de cada uno de estas estados de la direccion*/
-    uint8_t len;
-    /** cuantos bytes ya leimos */
-    uint8_t i;
-    // TODO: mover toda la logica al parser de auth, y delegar aca a ese parser
-    uint8_t uname[0xFF];
-    uint8_t passwd[0xFF];
+    monitor_error_invalid_data,
 };
 
 enum monitor_response_status {
-    status_ok = 0x00,
-    status_invalid_version = 0x01,
-    status_invalid_method = 0x02,
-    status_invalid_target = 0x03,
-    status_invalid_data = 0x04,
-    status_invalid_auth = 0x05,
+    monitor_status_succeeded                            = 0x00,
+    monitor_status_invalid_version                      = 0x01,
+    monitor_status_invalid_method                       = 0x02,
+    monitor_status_invalid_target                       = 0x03,
+    monitor_status_invalid_data                         = 0x04,
+    monitor_status_error_auth                           = 0x05,
+    monitor_status_server_error                         = 0x06,
+
+};
+
+
+enum monitor_method {
+    monitor_method_get    = 0x00,
+    monitor_method_config = 0x01,
+};
+
+enum monitor_target_get {
+    monitor_target_get_historic   = 0x00,
+    monitor_target_get_concurrent = 0x01,
+    monitor_target_get_transfered = 0x02,
+    monitor_target_get_proxyusers = 0x03,
+    monitor_target_get_adminusers = 0x04,
+};
+
+enum monitor_target_config {
+    monitor_target_config_pop3disector      = 0x00,
+    monitor_target_config_add_proxyuser     = 0x01,
+    monitor_target_config_delete_proxyuser  = 0x02,
+    monitor_target_config_add_admin         = 0x03,
+    monitor_target_config_delete_admin      = 0x04,
+};
+
+
+union monitor_target {
+    enum monitor_target_get    target_get;
+    enum monitor_target_config target_config;
+};
+
+
+enum config_disector_data {
+    disector_off    = 0x00,
+    disector_on     = 0x01,
+};
+
+struct config_add_proxy_user {
+    char        user[USERNAME_SIZE];
+    char        pass[PASSWORD_SIZE];
+};
+
+struct config_add_admin_user {
+    char        user[USERNAME_SIZE];
+    char        token[TOKEN_SIZE];
+};
+
+union data {
+    char                            user[USERNAME_SIZE]; // To delete proxy user or admin user
+    enum   config_disector_data     disector_data_params;
+    struct config_add_proxy_user    add_proxy_user_param;
+    struct config_add_admin_user    add_admin_user_param;
+};
+
+
+struct monitor {
+    uint8_t                 token[TOKEN_SIZE];
+    enum  monitor_method    method;
+    union monitor_target    target;
+    uint16_t                dlen;
+    union data              data;
+};
+
+
+struct monitor_parser {
+    struct monitor *monitor;
+    enum monitor_state state;
+    /** cuantos bytes tenemos que leer*/
+    uint8_t len;
+    /** cuantos bytes ya leimos */
+    uint8_t i;
 };
 
 /** inicializa el parser */
@@ -99,31 +176,27 @@ monitor_parser_init(struct monitor_parser *p);
 enum monitor_state
 monitor_consume(buffer *b, struct monitor_parser *p, bool *errored);
 
+
+
 /*
- * Permite distinguir a quien usa socks_hello_parser_feed si debe seguir 
+ * Permite distinguir a quien usa monitor_parser_feed si debe seguir 
  * enviando caracteres o no
  * 
  * En caso de haber terminado permite tambien saber si se debe a un error.
  */
 bool
-monitor_is_done(const enum request_state st, bool *errored);
+monitor_is_done(const enum monitor_state st, bool *errored);
 
 void
 monitor_close(struct monitor_parser *p);
 
 /*
- * serializa en buff una respuesta al request,
+ * serializa en buff una respuesta al request del protocolo de monitoreo,
  * 
  * Retorna la cantidad de bytes ocupados del buffer o -1 si no habia 
  * espacio suficiente.
  */
 extern int
-monitor_marshall(buffer *b,
-                const enum monitor_response_status status);
-
-
-/** convierte a errno en socks_response_status */
-enum socks_response_status
-errno_to_socks(int e);
+monitor_marshall(buffer *b, const enum monitor_response_status status, uint16_t dlen, void *data);
 
 #endif
--- src/server/monitor.c
+++ src/server/monitor.c
@@ -1,9 +1,33 @@
+/**
+ * monitor.c -- parser del request de protocolo nuestro de monitoreo
+ */
+#include <string.h> //memset
+#include <arpa/inet.h> //ntohs
+
 #include "../include/monitor.h"
+
+
+#define IS_ALNUM(x) (x>='a' && (x) <= 'z') || (x>='A' && x <= 'Z') || (x>='0' && x <= '9')
+
+static int separated = 0;
+
+static uint8_t combinedlen[2] = {0};
+
+static void
+remaining_set(struct monitor_parser *p, int len) {
+    p->i = 0;
+    p->len = len; 
+}
+
+static int 
+remaining_is_done(struct monitor_parser *p) {
+    return p->i >= p->len;
+}
 
 extern void
 monitor_parser_init(struct monitor_parser *p) {
     p->state = monitor_version;
-    memset(p->request, 0, sizeof(*(p->request)));
+    memset(p->monitor, 0, sizeof(*(p->monitor)));
 }
 
 static enum monitor_state
@@ -11,7 +35,7 @@ version(const uint8_t c, struct monitor_parser *p) {
     enum monitor_state next;
     switch (c) {
         case 0x01:
-            next = monitor_method;
+            next = monitor_token;
             break;
         default:
             next = monitor_error_unsupported_version;
@@ -22,54 +46,61 @@ version(const uint8_t c, struct monitor_parser *p) {
 }
 
 static enum monitor_state
+token(const uint8_t c, struct monitor_parser *p) {
+    p->monitor->token[p->i++] = c;
+    if (remaining_is_done(p))
+        return monitor_method;
+    return monitor_token;
+}
+
+static enum monitor_state
 method(const uint8_t c, struct monitor_parser *p) {
     enum monitor_state next;
-    switch (c) {
-        case method_get:
-        case method_config:
+
+    p->monitor->method = c;
+    switch (p->monitor->method) {
+        case monitor_method_get:
+        case monitor_method_config:
             next = monitor_target;
             break;
         default:
             next = monitor_error_unsupported_method;
             break;
     }
-    p->request->method = c;
-    return next;
-}
 
-static bool
-is_valid_pop3_disector(struct monitor_parser *p) {
-	// TODO
+    return next;
 }
 
 static enum monitor_state
 target(const uint8_t c, struct monitor_parser *p) {
     enum monitor_state next;
-    switch(p->request->method) {
-        case method_get:
+
+    switch(p->monitor->method) {
+        case monitor_method_get:
             switch (c) {
-                case target_get_historic:
-                case target_get_concurrent:
-                case target_get_transfered:
-                    next = monitor_dlen;
-					memset(&(p->request->target), 0, sizeof(int));
-					p->request->target.get = c;
+                case   monitor_target_get_historic:
+                case monitor_target_get_concurrent:
+                case monitor_target_get_transfered:
+                case monitor_target_get_proxyusers:
+                case monitor_target_get_adminusers:
+					p->monitor->target.target_get = c;
+                    next = monitor_done;
                     break;
                 default:
                     next = monitor_error_unsupported_target;
                     break;
             }
             break;
-        case method_config:
+        case monitor_method_config:
             switch (c) {
-                case target_config_pop3disector:
+                case monitor_target_config_pop3disector:
+                case monitor_target_config_add_proxyuser:
+                case monitor_target_config_delete_proxyuser:
+                case monitor_target_config_add_admin:
+                case monitor_target_config_delete_admin:
+					p->monitor->target.target_config = c;
+                    remaining_set(p, 2); // vamos a leer 2 bytes para el dlen
                     next = monitor_dlen;
-					memset(&(p->request->target), 0, sizeof(struct monitor_req_target_config));
-					struct monitor_req_target_config config = {
-						.option	= c,
-						.isValid = is_valid_pop3_disector,
-					};
-					memcpy(p->request->target.config, config, sizeof(struct monitor_req_target_config));
                     break;
                 default:
                     next = monitor_error_unsupported_target;
@@ -84,61 +115,159 @@ target(const uint8_t c, struct monitor_parser *p) {
     return next;
 }
 
+//Si llego aca estamos con un target de tipo config
 static enum monitor_state
 dlen(const uint8_t c, struct monitor_parser *p) {
-    p->request->len = c;
-    p->i = 0;
-    p->len = c;
-    return monitor_data;
+    enum monitor_state next;
+
+    if (p->i == 0) {
+        combinedlen[p->i++] = c;
+        next = monitor_dlen;
+    }
+    else if (p->i == 1) {
+        combinedlen[p->i++] = c;
+        next = monitor_dlen;
+    } 
+    else {
+        p->len = ntohs(*(uint16_t*)combinedlen); // Para evitar problemas de endianness armo el uint16 de dlen segun el endianness del sistema. (Suponiendo que me los manda en network order "bigendean")
+        switch (p->monitor->target.target_config) {
+            case monitor_target_config_pop3disector:
+                next = monitor_data;
+                break;
+            case monitor_target_config_add_proxyuser:
+                remaining_set(p, p->len);
+                next = monitor_data;
+                break;
+            case monitor_target_config_delete_proxyuser:
+                remaining_set(p, p->len); 
+                next = monitor_data;
+                break;
+            case monitor_target_config_add_admin:
+                remaining_set(p, p->len);
+                next = monitor_data;
+                break;
+            case monitor_target_config_delete_admin:
+                remaining_set(p, p->len);
+                next = monitor_data;
+                break;
+            default:
+                next = monitor_error;
+                break;
+        }
+    }
+    
+    return next;
 }
 
 static enum monitor_state
 data(const uint8_t c, struct monitor_parser *p) {
-    p->request->data[p->i++] = c;
-    if (p->i == p->len) {
-        if (p->request->target->config.isValid(p)) 
-		// validar data
-        return monitor_ulen;
-		return monitor_error_unsupported_data
+    enum monitor_state next;
+
+    switch(p->monitor->target.target_config) { 
+        case monitor_target_config_pop3disector:
+            p->monitor->data.disector_data_params = c;
+            next = monitor_done;
+            break;
+        
+        case monitor_target_config_add_proxyuser: 
+            // Si el primer caracter es 0 directamente tiro error ya que el usuario no puede ser vacio
+            if (p->i == 0 && c == 0) {
+                next = monitor_error_invalid_data;
+                break;
+            }
+
+            if (remaining_is_done(p)) {
+                p->monitor->data.add_proxy_user_param.pass[p->i++] = 0; // null terminated para password
+                next = monitor_done;
+                break;
+            }
+
+            if (IS_ALNUM(c)) {
+                if (separated == 0) {
+                    p->monitor->data.add_proxy_user_param.user[p->i++] = c;
+                } else {
+                    p->monitor->data.add_proxy_user_param.pass[p->i++] = c;
+                }
+            } else if (c == 0 && separated == 0) { // primer separador \0 pongo el null terminated en el username
+                p->monitor->data.add_proxy_user_param.user[p->i++] = c;
+                separated = 1;
+            } else { // Si no es alfanumerico ni fue el primer 0 separador entonces no es un dato valido
+                next = monitor_error_invalid_data;
+                break;
+            }
+            
+            next = monitor_data;
+            break;
+
+        case monitor_target_config_delete_proxyuser:
+            if (remaining_is_done(p)) {
+                p->monitor->data.user[p->i++] = 0; // null terminated para username
+                next = monitor_done;
+                break;
+            }
+        
+            if (IS_ALNUM(c)) {
+                p->monitor->data.user[p->i++] = c;
+            } else {
+                next = monitor_error_invalid_data;
+                break;
+            } 
+
+            next = monitor_data;
+            break;
+
+        case monitor_target_config_add_admin:
+            // Si el primer caracter es 0 directamente tiro error ya que el usuario no puede ser vacio
+            if (p->i == 0 && c == 0) {
+                next = monitor_error_invalid_data;
+                break;
+            }
+
+            if (remaining_is_done(p)) {
+                p->monitor->data.add_admin_user_param.token[p->i++] = 0; // null terminated para password
+                next = monitor_done;
+                break;
+            }
+
+            if (IS_ALNUM(c)) {
+                if (separated == 0) {
+                    p->monitor->data.add_admin_user_param.user[p->i++] = c;
+                } else {
+                    p->monitor->data.add_admin_user_param.token[p->i++] = c;
+                }
+            } else if (c == 0 && separated == 0) { // primer separador \0 pongo el null terminated en el username
+                p->monitor->data.add_admin_user_param.user[p->i++] = c;
+                separated = 1;
+            } else { // Si no es alfanumerico ni fue el primer 0 separador entonces no es un dato valido
+                next = monitor_error_invalid_data;
+                break;
+            }
+            
+            next = monitor_data;
+            break;
+
+        case monitor_target_config_delete_admin:
+             if (remaining_is_done(p)) {
+                p->monitor->data.user[p->i++] = 0; // null terminated para username
+                next = monitor_done;
+                break;
+            }
+        
+            if (IS_ALNUM(c)) {
+                p->monitor->data.user[p->i++] = c;
+            } else {
+                next = monitor_error_invalid_data;
+                break;
+            } 
+
+            next = monitor_data;
+            break;
     }
-    return monitor_data;
+
+
+    return next;
 }
 
-// TODO: mover toda la logica al parser de auth, y delegar aca a ese parser
-static enum monitor_state
-ulen(const uint8_t c, struct monitor_parser *p) {
-    p->i = 0;
-    p->len = c;
-    return monitor_uname;
-}
-
-static enum monitor_state
-uname(const uint8_t c, struct monitor_parser *p) {
-    p->uname[p->i++] = c;
-    if (p->i == p->len)
-        return monitor_plen;
-    return monitor_uname;
-}
-
-static enum monitor_state
-plen(const uint8_t c, struct monitor_parser *p) {
-    p->i = 0;
-    p->len = c;
-    return monitor_passwd;
-}
-
-static enum monitor_state
-passwd(const uint8_t c, struct monitor_parser *p) {
-    p->passwd[p->i++] = c;
-    if (p->i == p->len)
-        return monitor_authenticate;
-    return monitor_passwd;
-}
-
-static enum monitor_state
-authenticate(const uint8_t c, struct monitor_parser *p) {
-	// TODO
-}
 
 /** entrega un byte al parser, retorna true si se llego al final */
 static enum monitor_state 
@@ -148,6 +277,9 @@ monitor_parser_feed(struct monitor_parser *p, const uint8_t c) {
     switch(p->state) {
         case monitor_version:
             next = version(c, p);
+            break;
+        case monitor_token:
+            next = token(c, p);
             break;
         case monitor_method:
             next = method(c, p);
@@ -161,28 +293,13 @@ monitor_parser_feed(struct monitor_parser *p, const uint8_t c) {
         case monitor_data:
             next = data(c, p);
             break;
-        case monitor_ulen:
-            next = ulen(c, p);
-            break;
-        case monitor_uname:
-            next = uname(c, p);
-            break;
-        case monitor_plen:
-            next = plen(c, p);
-            break;
-        case monitor_passwd:
-            next = passwd(c, p);
-            break;
-		case monitor_authenticate:
-            next = authenticate(c, p);
-            break;
         case monitor_done:
         case monitor_error:
         case monitor_error_unsupported_version:
+        case monitor_error_invalid_token:
         case monitor_error_unsupported_method:
         case monitor_error_unsupported_target:
-        case monitor_error_unsupported_data:
-        case monitor_error_unsupported_auth:
+        case monitor_error_invalid_data:
             next = p->state;
             break;
         default:
@@ -191,4 +308,57 @@ monitor_parser_feed(struct monitor_parser *p, const uint8_t c) {
     }
 
     return p->state = next;
+}
+
+extern bool
+monitor_is_done(const enum monitor_state st, bool *errored) {
+    if (st >= monitor_error && errored != 0)
+        *errored = true;
+    return st >= monitor_done;
+}
+
+extern enum monitor_state
+monitor_consume(buffer *b, struct monitor_parser *p, bool *errored) {
+    enum monitor_state st = p->state;
+
+    while (buffer_can_read(b)) {
+        const uint8_t c = buffer_read(b);
+        st = monitor_parser_feed(p, c); // cargamos 1 solo byte
+        if (monitor_is_done(st, errored))
+            break;
+    }
+    return st;
+}
+
+extern void
+monitor_close(struct monitor_parser *p) {
+    // nada que hacer
+}
+
+
+extern int
+monitor_marshall(buffer *b, const enum monitor_response_status status, uint16_t dlen, void *data) {
+    // llenar status y dlen primero, checkeando el espacio que hay en el buffer (si te quedas sin espacio en el buffer retornas -1)
+    size_t n;
+    buffer_write_ptr(b, &n);
+
+    if (n < (size_t) dlen + 3)
+        return -1;
+
+    
+    uint8_t array[2];
+    array[0]= dlen & 0xff; // menos significativa
+    array[1]=(dlen >> 8);  // mas significativa
+
+    buffer_write(b, status);
+    buffer_write(b, array[1]);
+    buffer_write(b, array[0]);
+
+
+    uint8_t *databytes = (uint8_t *) data;
+    for (uint16_t i = 0; i < dlen; i++) {
+        buffer_write(b, databytes[i]);
+    }
+
+    return dlen + 3;
 }
