bcf9654f0f9145a8272c80a9123c3731ff91106e
gbaliarda <gbaliarda@itba.edu.ar>
2022-06-20 19:47:35 -0300

    refactor: client.c modularization

--- src/include/client.h
+++ src/include/client.h
@@ -4,4 +4,14 @@
 #define MAX_BYTES_DATA          65535
 #define MAX_CLIENT_REQUESTS     20
 
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include "clientargs.h"
+#include "clientrequest.h"
+
+void handle_get_ok_status(struct client_request_args arg, uint8_t *buf, uint8_t *combinedlen, uint8_t *numeric_data_array, uint32_t *numeric_response);
+
+void handle_config_ok_status(struct client_request_args arg);
+
 #endif
